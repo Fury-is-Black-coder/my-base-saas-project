f18ce17145fc9e74eb2ca619069a5f6254a19a74
motobudrex-dev <157002112+motobudrex-dev@users.noreply.github.com>
2025-05-31 12:04:01 +0200

    up

---
+++ backend/src/main/java/com/devcolibri/backend/service/JwtService.java
@@ -0,0 +1,42 @@
+package com.devcolibri.backend.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "twojSuperTajnyKluczJWT"; // Zmien na mocny sekret
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plusSeconds(3600)))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
+
